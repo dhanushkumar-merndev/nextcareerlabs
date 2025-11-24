6e898d4b26fd2bc8f14df045cfdda9731c3191e3
dhanushkumar-merndev <dhanushkumar.merndev@gmail.com>
2025-11-25 00:58:44 +0530

    postinstalled edit 4

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,22 +3,18 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z
-      .string()
-      .startsWith("postgresql://", { message: "Must be a PostgreSQL URL" }),
+    DATABASE_URL: z.string().min(1), // FIXED
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     GOOGLE_CLIENT_ID: z.string().min(1),
-    BETTER_AUTH_URL: z.string().url(),
+    BETTER_AUTH_URL: z.string().min(1), // FIXED
     BETTER_AUTH_SECRET: z.string().min(1),
     RESEND_API_KEY: z.string().min(1),
     ARCJET_KEY: z.string().min(1),
-
     AWS_ACCESS_KEY_ID: z.string().min(1),
     AWS_SECRET_ACCESS_KEY: z.string().min(1),
     AWS_ENDPOINT_URL_S3: z.string().min(1),
     AWS_ENDPOINT_URL_IAM: z.string().min(1),
     AWS_REGION: z.string().min(1),
-
     STRIPE_SECRET_KEY: z.string().min(1),
     STRIPE_WEBHOOK_SECRET: z.string().min(1),
   },
